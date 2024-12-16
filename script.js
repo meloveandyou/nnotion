f17c5ab6a14da083c47ebae1273d1fbaa4d1c91f
meloveandyou <fragolatadigitalart@gmail.com>
2024-12-16 15:43:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Counter
+let counter = 0;
+function incrementCounter() {
+  counter++;
+  document.getElementById("counter").innerText = counter;
+}
+function decrementCounter() {
+  counter--;
+  document.getElementById("counter").innerText = counter;
+}
+
+// Add other widget features here...
